2fbfa155f04b520985dea6afa65cf5020aebc1fe
Esther <11yekinie@gmail.com>
2025-01-03 23:27:53 +0000

    contentful takeover

--- src/app/home.component.ts
+++ src/app/home.component.ts
@@ -15,9 +15,14 @@ export class Home implements OnInit {
   constructor(private contentfulService: ContentfulService) {}
 
   homePage$: Observable<any> | undefined;
+  backgroundImage: string = '';
 
   ngOnInit(): void {
     const contentTypeId = 'landingPage';
     this.homePage$ = this.contentfulService.getEntriesByContentType(contentTypeId);
+
+    this.homePage$.subscribe((data: any) => {
+      this.backgroundImage = data.items[0].fields.backgroundImage.fields.file.url || ''
+    })
   }
 }
